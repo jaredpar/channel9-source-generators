b79ce0b605b7debf05fa689874bdb530c821e335
Jared Parsons <jaredpparsons@gmail.com>
2021-01-27 15:33:19 -0800

    Got a basic test loop setup

--- Generators/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Generators
-{
-    public class Class1
-    {
-    }
-}
---
+++ GeneratorsUnitTests/Class1.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Text;
+using System;
+using Xunit;
+// TODO: delete and put the logic for embedding here.
+using Roslyn.CodeDom;
+using Microsoft.CodeAnalysis;
+using System.Linq;
+
+namespace Generators.UnitTests
+{
+    public sealed class AutoEqualityTests
+    {
+        public GeneratorDriverRunResult GetRunResult(string sourceCode)
+        {
+            var options = new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary);
+
+            var compilation = CSharpCompilation.Create(
+                assemblyName: "TestAssembly",
+                syntaxTrees: new[] { CSharpSyntaxTree.ParseText(sourceCode) },
+                options: options).WithFrameworkReferences(TargetFramework.NetCoreApp31);
+
+            var driver = CSharpGeneratorDriver.Create(new AutoEqualityGenerator());
+            return driver
+                .RunGenerators(compilation)
+                .GetRunResult();
+        }
+
+        public SyntaxTree GetGeneratedTree(string sourceCode)
+        {
+            var result = GetRunResult(sourceCode);
+            return result.GeneratedTrees.Single(x => x.FilePath.Contains("GeneratedEquality"));
+        }
+
+        [Fact]
+        public void Simple()
+        {
+            var source = @"
+using System;
+";
+
+            var syntaxTree = GetGeneratedTree(source);
+            Assert.Equal("", syntaxTree.ToString());
+
+
+
+
+
+
+
+        }
+    }
+}
