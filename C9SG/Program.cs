be7efdf65737281bf98444690d07460e4a000456
Jared Parsons <jaredpparsons@gmail.com>
2021-01-27 13:21:05 -0800

    Skeleton dump

---
+++ C9SG/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C9SG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
