1e0258b49f6a1728f1a283a191e92a87e45bc044
Jared Parsons <jaredpparsons@gmail.com>
2021-01-28 08:57:49 -0800

    Equality analyzer

---
+++ Generators/EqualityAnalyzer.cs
@@ -0,0 +1,158 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Text;
+
+namespace Generators
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public sealed class EqualityAnalyzer : DiagnosticAnalyzer
+    {
+        public static DiagnosticDescriptor DiagnosticNeedOperatorEqauls = new(
+            "JP001",
+            title: "Add equality operators",
+            messageFormat: "Type {0} needs both == and != operators",
+            category: "Equality",
+            defaultSeverity: DiagnosticSeverity.Warning,
+            isEnabledByDefault: true);
+
+        public static DiagnosticDescriptor DiagnosticNeedImplementIEquatable = new(
+            "JP002",
+            title: "Implement IEquatable<T>",
+            messageFormat: "Type {0} to implement IEquatable<T>",
+            category: "Equality",
+            defaultSeverity: DiagnosticSeverity.Warning,
+            isEnabledByDefault: true);
+
+        public static DiagnosticDescriptor DiagnosticNeedStrongEquals = new(
+            "JP002",
+            title: "Need strongly typed Equals",
+            messageFormat: "Type {0} needs a strongly typed Equals method",
+            category: "Equality",
+            defaultSeverity: DiagnosticSeverity.Warning,
+            isEnabledByDefault: true);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(
+            DiagnosticNeedOperatorEqauls,
+            DiagnosticNeedImplementIEquatable,
+            DiagnosticNeedStrongEquals);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.EnableConcurrentExecution();
+            context.RegisterCompilationStartAction(context =>
+            {
+                var compilation = context.Compilation;
+                var typeIEquatableT = compilation.GetTypeByMetadataName(typeof(IEquatable<>).FullName!) as INamedTypeSymbol;
+                if (typeIEquatableT is object)
+                {
+                    var analyzer = new Analyzer(compilation, typeIEquatableT);
+                    context.RegisterSymbolAction(analyzer.OnSymbol, SymbolKind.NamedType);
+                }
+            });
+        }
+
+        private sealed class Analyzer
+        {
+            public Compilation Compilation { get; }
+            public INamedTypeSymbol TypeIEquatableT { get; }
+
+            public Analyzer(Compilation compilation, INamedTypeSymbol typeIEquatableT)
+            {
+                Compilation = compilation;
+                TypeIEquatableT = typeIEquatableT;
+            }
+
+            public void OnSymbol(SymbolAnalysisContext context)
+            {
+                if (!(context.Symbol is INamedTypeSymbol namedTypeSymbol &&
+                    namedTypeSymbol.TypeKind is TypeKind.Class or TypeKind.Struct))
+                {
+                    return;
+                }
+
+                if (HasEqualsQualities(namedTypeSymbol))
+                {
+                    if (!ImplementsIEquatableT(namedTypeSymbol))
+                    {
+                        context.ReportDiagnostic(Diagnostic.Create(
+                            DiagnosticNeedImplementIEquatable,
+                            namedTypeSymbol.Locations[0],
+                            namedTypeSymbol.Name));
+                    }
+
+                    if (!HasEqualityOperators(namedTypeSymbol))
+                    {
+                        context.ReportDiagnostic(Diagnostic.Create(
+                            DiagnosticNeedOperatorEqauls,
+                            namedTypeSymbol.Locations[0],
+                            namedTypeSymbol.Name));
+                    }
+
+                    if (!HasStronglyTypedEquals(namedTypeSymbol))
+                    {
+                        context.ReportDiagnostic(Diagnostic.Create(
+                            DiagnosticNeedStrongEquals,
+                            namedTypeSymbol.Locations[0],
+                            namedTypeSymbol.Name));
+                    }
+                }
+            }
+
+            private bool ImplementsIEquatableT(INamedTypeSymbol namedTypeSymbol)
+            {
+                if (namedTypeSymbol.Interfaces.Length > 0)
+                {
+                    foreach (var i in namedTypeSymbol.Interfaces)
+                    {
+                        if (i.OriginalDefinition.Equals(TypeIEquatableT, SymbolEqualityComparer.Default))
+                        {
+                            return true;
+                        }
+                    }
+                }
+
+                return false;
+            }
+
+            private bool HasStronglyTypedEquals(INamedTypeSymbol namedTypeSymbol)
+            {
+                foreach (var methodSymbol in namedTypeSymbol.GetMembers("Equals").OfType<IMethodSymbol>())
+                {
+                    if (methodSymbol.Parameters.Length == 1 &&
+                        namedTypeSymbol.Equals(methodSymbol.Parameters[0].Type, SymbolEqualityComparer.Default) &&
+                        methodSymbol.ReturnType?.SpecialType == SpecialType.System_Boolean)
+                    {
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+
+            private bool HasEqualityOperators(INamedTypeSymbol namedTypeSymbol) =>
+                namedTypeSymbol.GetMembers("op_Equality").Length > 0 &&
+                namedTypeSymbol.GetMembers("op_Inequality").Length > 0;
+
+            private bool HasEqualsQualities(INamedTypeSymbol namedTypeSymbol)
+            {
+                if (ImplementsIEquatableT(namedTypeSymbol))
+                {
+                    return true;
+                }
+
+                if (HasEqualityOperators(namedTypeSymbol) ||
+                    namedTypeSymbol.GetMembers("Equals").Length > 0 ||
+                    namedTypeSymbol.GetMembers("GetHashCode").Length > 0)
+                {
+                    return true;
+                }
+
+                return false;
+            }
+        }
+    }
+}
---
+++ GeneratorsUnitTests/EqualityAnalyzerUnitTests.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Roslyn.CodeDom;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Generators.UnitTests
+{
+    public sealed class EqualityAnalyzerUnitTests
+    {
+        public Compilation GetCompilation(string sourceCode) =>
+            GetCompilation(new[] { CSharpSyntaxTree.ParseText(sourceCode) });
+
+        public Compilation GetCompilation(IEnumerable<SyntaxTree> syntaxTrees)
+        {
+            var options = new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary);
+
+            var compilation = CSharpCompilation.Create(
+                assemblyName: "TestAssembly",
+                syntaxTrees: syntaxTrees,
+                options: options).WithFrameworkReferences(TargetFramework.NetCoreApp31);
+
+            return compilation;
+        }
+
+        public async Task<List<Diagnostic>> GetDiagnosticsAsync(string sourceCode)
+        {
+            var compilation = new CompilationWithAnalyzers(
+                GetCompilation(sourceCode),
+                ImmutableArray.Create<DiagnosticAnalyzer>(new EqualityAnalyzer()),
+                new CompilationWithAnalyzersOptions(
+                    new AnalyzerOptions(ImmutableArray<AdditionalText>.Empty),
+                    (ex, _, _) => Assert.True(false, ex.Message),
+                    concurrentAnalysis: false,
+                    logAnalyzerExecutionTime: false));
+            var diagnostics = await compilation
+                .GetAnalyzerDiagnosticsAsync()
+                .ConfigureAwait(false);
+            return diagnostics
+                .OrderBy(x => x.Id)
+                .ToList();
+        }
+
+        [Fact]
+        public async Task NeedsOperators()
+        {
+            var source = @"
+using System;
+using System.Collections.Generic;
+
+class C : IEquatable<C>
+{
+    public bool Equals(C other) => true;
+}
+";
+
+            var diagnostics = await GetDiagnosticsAsync(source).ConfigureAwait(false);
+            Assert.Single(diagnostics);
+            Assert.Equal(EqualityAnalyzer.DiagnosticNeedOperatorEqauls, diagnostics[0].Descriptor);
+        }
+
+        [Fact]
+        public async Task NeedsStronglyTypedEquals()
+        {
+            var source = @"
+using System;
+using System.Collections.Generic;
+
+class C : IEquatable<C>
+{
+    public static bool operator==(C left, C right) => true;
+    public static bool operator!=(C left, C right) => true;
+}
+";
+
+            var diagnostics = await GetDiagnosticsAsync(source).ConfigureAwait(false);
+            Assert.Single(diagnostics);
+            Assert.Equal(EqualityAnalyzer.DiagnosticNeedStrongEquals, diagnostics[0].Descriptor);
+        }
+
+        [Fact]
+        public async Task NeedsInterface()
+        {
+            var source = @"
+using System;
+using System.Collections.Generic;
+
+class C
+{
+    public bool Equals(C other) => true;
+    public static bool operator==(C left, C right) => true;
+    public static bool operator!=(C left, C right) => true;
+}
+";
+
+            var diagnostics = await GetDiagnosticsAsync(source).ConfigureAwait(false);
+            Assert.Single(diagnostics);
+            Assert.Equal(EqualityAnalyzer.DiagnosticNeedImplementIEquatable, diagnostics[0].Descriptor);
+        }
+
+        [Fact]
+        public async Task NeedsNothing()
+        {
+            var source = @"
+using System;
+using System.Collections.Generic;
+
+class C : IEquatable<C>
+{
+    public bool Equals(C other) => true;
+    public static bool operator==(C left, C right) => true;
+    public static bool operator!=(C left, C right) => true;
+}
+";
+
+            var diagnostics = await GetDiagnosticsAsync(source).ConfigureAwait(false);
+            Assert.Empty(diagnostics);
+        }
+    }
+}
